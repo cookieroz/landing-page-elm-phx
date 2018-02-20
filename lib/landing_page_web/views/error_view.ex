90829daf8545f1e28ba52439f0884f1e289b28f9
cookieroz <cookieroz@gmail.com>
2018-02-20 05:06:42 -0800

    Saving progress, finished part 1

---
+++ lib/landing_page_web/views/error_view.ex
@@ -0,0 +1,23 @@
+defmodule LandingPageWeb.ErrorView do
+  use LandingPageWeb, :view
+
+  import LandingPageWeb.ErrorHelpers
+
+  def render("404.html", _assigns) do
+    "Page not found"
+  end
+
+  def render("500.html", _assigns) do
+    "Internal server error"
+  end
+
+  def render("error.json", %{changeset: changeset}) do
+    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  end
+
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
+    render "500.html", assigns
+  end
+end
